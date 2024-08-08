b8dc51e26ea910fca4c7e518058db7ad829880dd
rachana-k-kulkarni <rachana.k.kulkarni@gmail.com>
2024-08-08 23:06:38 +0530

    Update Helloworldmodule.c

--- Hello_world_LKM/Helloworldmodule.c
+++ Hello_world_LKM/Helloworldmodule.c
@@ -1,1 +1,25 @@
-#include
+#include <linux/module.h>
+#include <linux/init.h>
+
+/* Meta Information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rachana K Kulkarni");
+MODULE_DESCRIPTION("A hello world LKM following Johannes's tutorial");
+
+/**
+ *Function called when module is loaded into the kernel
+ */
+static int __init my_init(void) {
+	printk("Hello, Kernel!\n");
+	return 0;
+}
+
+/**
+ *Function called when module is removed from the kernel
+ */
+static void __exit my_exit(void) {
+	printk("Goodbye, Kernel\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
